4c5e76b1642ecdafdece74acd8482335008f660c
TheBestUser01234567890 <98957578+TheBestUser01234567890@users.noreply.github.com>
2022-02-23 21:12:08 +0200

    Add files via upload

---
+++ gvmcore.cpp
@@ -0,0 +1,14 @@
+/**********************************
+* gvmcore.c: core for all GVMs
+**********************************/
+#include <stdio.h>
+#include "gvm.h"
+#include "gvmcontrol.h"
+#include "gvmcontrol_dev.h"
+#include "devtrack.h"
+
+#define __GVM_CORE__
+
+struct gvm {
+
+};
